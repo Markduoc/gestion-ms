69d67fd00ecc8f6954bd25f20ddae473074d0249
Markduoc <marc.corrales@duocuc.cl>
2025-05-24 22:17:57 -0400

    Initial commit

---
+++ src/test/java/gestion/EcoMarket/EcoMarketApplicationTests.java
@@ -0,0 +1,13 @@
+package gestion.EcoMarket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcoMarketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
