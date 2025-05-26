5ae60fdf232fc1b8e4638fafaee9c258f2d85fca
JorgeToledoIporre <jorg.toledo@duocuc.cl>
2025-05-26 15:21:10 -0400

    import incorrecto

--- src/main/java/gestion/EcoMarket/EcoMarketApplication.java
+++ src/main/java/gestion/EcoMarket/EcoMarketApplication.java
@@ -1,5 +1,5 @@
 package gestion.EcoMarket;
-
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
