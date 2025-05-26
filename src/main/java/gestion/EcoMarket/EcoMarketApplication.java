b3fb4831a63ec727727c1b0ba61e462e93cb3c0e
JorgeToledoIporre <jorg.toledo@duocuc.cl>
2025-05-26 15:11:24 -0400

    se hacen secretas las claves

--- src/main/java/gestion/EcoMarket/EcoMarketApplication.java
+++ src/main/java/gestion/EcoMarket/EcoMarketApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class EcoMarketApplication {
 
 	public static void main(String[] args) {
+		Dotenv.load();
 		SpringApplication.run(EcoMarketApplication.class, args);
 	}
 
