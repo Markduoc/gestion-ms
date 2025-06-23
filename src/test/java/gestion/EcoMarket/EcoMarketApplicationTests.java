c3563205353457c501e8905b6719e187df26897b
JorgeToledoIporre <jorg.toledo@duocuc.cl>
2025-06-23 14:37:16 -0400

    Update EcoMarketApplicationTests.java

--- src/test/java/gestion/EcoMarket/EcoMarketApplicationTests.java
+++ src/test/java/gestion/EcoMarket/EcoMarketApplicationTests.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest(classes = EcoMarketApplication.class)
+@org.springframework.test.context.ActiveProfiles("test")
 class EcoMarketApplicationTests {
 
 	@Test
